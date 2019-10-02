ab1b2d88f9ea826f7ddfdb1f7860ad6b54acc06f
andres-linares <andres.linares.971122@gmail.com>
2019-10-02 15:16:05 -0500

    renamed circle perimeter function

--- Calculator.java
+++ Calculator.java
@@ -119,7 +119,7 @@ public class Calculator {
 	 *            the radius
 	 * @return the perimeter of a circle with radius r.
 	 */
-	public double cirPer(double r) {
+	public double circlePerimeter(double r) {
 		return Math.PI * r * r;
 	}
 
